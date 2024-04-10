5ab4f270e0a5ab781a84f4aa26f9abd118e9833b
JoRyJuKy <joryjuky@gmail.com>
2024-04-10 17:38:45 -0400

    Small changes to setup again

--- run_setup.py
+++ run_setup.py
@@ -1,11 +1,10 @@
-import os, json, asyncio
-import dotenv
+import os, json
+import dotenv; dotenv.load_dotenv()
 
-from misc.colors import KAVANI_COLOR; dotenv.load_dotenv()
-
+from misc.colors import KAVANI_COLOR
 from interactions import Client, Embed, EmbedFooter, Intents, listen
 
-input("Please make sure to edit this file with correct role, channel, and command IDs! Press enter to continue.")
+input("Please ensure config.json is pre-updated with thread, channel, and role IDs. Press enter to continue.")
 
 bot = Client(intents=Intents.DEFAULT)
 
@@ -13,14 +12,10 @@ bot = Client(intents=Intents.DEFAULT)
 with open(os.path.join(os.getcwd(), "config.json"), "r") as config_file:
     bot.config = json.loads(config_file.read())
 
-notif_role = 856889574638616587
-notif_thread = 1223631958953689160
-role_get_channel = 1221830865928323092
-logs_thread = 1223294878440493096
-
-timer_id = 0
-capturable_id = 0
-capture_id = 0
+notif_role = bot.config["notify_role"]
+notif_thread = bot.config["alerts_thread"]
+logs_thread = bot.config["logging_thread"]
+role_get_channel = int(input("Input reactions role channel id: "))
 
 description = f"""Welcome to the Kavani Mandate SystemTracker v2!
 This bot helps track timers and capturable systems for faction warfare.
@@ -44,11 +39,15 @@ async def on_ready():
         footer=EmbedFooter("Tip: All timestamps are automatically converted to your local timezone")
     )
     embed.add_field("Commands", value=cmd_description)
-    await channel.send(embed=embed) #type:ignore
+    help_msg = await channel.send(embed=embed) #type:ignore
     embed = Embed(title="Setup...")
-    await channel.send(embed=embed) #type:ignore
-
-    print("Setup finished! Make sure to update config.json with new message IDs.")
-    print("In particular, the `help_` and `main_message` will need changing.")
+    main_msg = await channel.send(embed=embed) #type:ignore
     await bot.stop()
+
+    bot.config["help_message"] = help_msg.id
+    bot.config["main_message"] = main_msg.id
+
+    with open(os.path.join(os.getcwd(), "config.json"), "w") as config_file:
+        config_file.write(json.dumps(bot.config, indent=4))
+
 bot.start(os.getenv("DISCORD_TOKEN"))
